74b294743bac4c2ca904d8013e2e3baef959498d
Bruno Pedrazza <pedrazzabruno@gmail.com>
2026-03-21 21:37:51 -0300

    feat: [US-001] - Show full price including decimals

--- js/bitcoinprice.js
+++ js/bitcoinprice.js
@@ -9,9 +9,9 @@ async function updatePrice() {
         const responseBody = await response.text()
         const bodyObj = JSON.parse(responseBody)
         const price = bodyObj.data.amount
-        document.title = currentCoin + " $" + price.split(".")[0]
+        document.title = currentCoin + " $" + price
         document.getElementById("coin-label").textContent = currentCoin;
-        document.getElementById("btc-price-display").textContent = "$" + price.split(".")[0];
+        document.getElementById("btc-price-display").textContent = "$" + price;
     } catch (e) {
         if (currentCoin !== "BTC") {
             currentCoin = "BTC";
